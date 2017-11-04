b4f00e9b160a1cdb6326eac96ddcc896884dddef
Shoumyo Chakravorti <shoumyoarchit@gmail.com>
2017-11-04 00:59:16 -0400

    Check win working

--- app/src/main/java/comschakravorti21/github/connectfourai/GameBoard.java
+++ app/src/main/java/comschakravorti21/github/connectfourai/GameBoard.java
@@ -1,6 +1,7 @@
 package comschakravorti21.github.connectfourai;
 
 import android.media.Image;
+import android.util.Log;
 import android.widget.ImageButton;
 
 /**
@@ -11,6 +12,9 @@ public class GameBoard {
 
     public static final int ROWS = 6;
     public static final int COLUMNS = 7;
+    public static final int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
+    public static final int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+
     private int[][] gameState;
     private ImageButton[][] buttons;
     //0 = empty, 1 = blue, 2 = red
@@ -53,6 +57,39 @@ public class GameBoard {
         return gameState[row][col];
     }
 
+    public boolean checkWin(int row, int col, int player) {
+        for(int i = 0, r = row, c = col ; i < dx.length/2 && i < dy.length/2;
+                i++, r = row, c = col) {
+
+            int x = dx[i];
+            int y = dy[i];
+
+            int count = 0;
+            while(r < ROWS && r >= 0 && c < COLUMNS && c >= 0
+                    && count < 4 && gameState[r][c] == player) {
+                count++;
+                //Log.d("COUNT, LOOP 1", "" + count);
+                r += y;
+                c += x;
+            }
+
+            r = row - y;
+            c = col - x;
+            while(r < ROWS && r >= 0 && c < COLUMNS && c >= 0
+                    && count < 4 && gameState[r][c] == player) {
+
+                count++;
+                //Log.d("COUNT, LOOP 2", "" + count);
+                r -= y;
+                c -= x;
+            }
+
+            if(count == 4)
+                return true;
+        }
+
+        return false;
+    }
     
 
 }
--- app/src/main/java/comschakravorti21/github/connectfourai/MainActivity.java
+++ app/src/main/java/comschakravorti21/github/connectfourai/MainActivity.java
@@ -2,6 +2,7 @@ package comschakravorti21.github.connectfourai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
@@ -38,7 +39,15 @@ public class MainActivity extends AppCompatActivity {
                 for(int i = pieces.length - 1; i >= 0; i--) {
                     //Place the piece if its empty
                     if(gameboard.getState(i, col) == 0) {
+
                         gameboard.placePiece(i, col, player);
+
+                        if(gameboard.checkWin(i, col, player)) {
+                            Log.d("Check Win", "TRUE");
+                        } else{
+                            Log.d("Check Win", "FALSE");
+                        }
+
                         player = -player;
                         break;
                     }
