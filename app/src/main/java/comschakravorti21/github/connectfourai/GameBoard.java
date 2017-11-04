93c20bbdf0cd1591e5e4b17b18945e3121fc0b41
yoloducks <atharva.kench@gmail.com>
2017-11-04 00:08:22 -0400

    .

--- app/src/main/java/comschakravorti21/github/connectfourai/GameBoard.java
+++ app/src/main/java/comschakravorti21/github/connectfourai/GameBoard.java
@@ -1,5 +1,8 @@
 package comschakravorti21.github.connectfourai;
 
+import android.media.Image;
+import android.widget.ImageButton;
+
 /**
  * Created by Athu on 11/3/2017.
  */
@@ -9,6 +12,7 @@ public class GameBoard {
     public static final int ROWS = 6;
     public static final int COLUMNS = 7;
     private int[][] gameState;
+    private ImageButton[][] buttons;
     //0 = empty, 1 = blue, 2 = red
 
 
@@ -18,6 +22,28 @@ public class GameBoard {
                 gameState[i][j] = 0;
             }
         }
+    }
+
+    //returns row that it was placed in
+    //@param playerNum, 1 if player 1,     -1 if player 2
+    public int placePiece(int row, int col, int playerNum) {
+        gameState[row][col] = playerNum;
+
+        if(playerNum == MainActivity.PLAYER_1) {
+            this.changePieceColor(row, col, R.mipmap.piece_yellow);
+        } else {
+            this.changePieceColor(row, col, R.mipmap.piece_red);
+        }
+
+        return row;
+    }
+
+    public void setButtonImg(int row, int col, ImageButton button){
+        buttons[row][col] = button;
+    }
+
+    public void changePieceColor(int row, int col, int res){
+        buttons[row][col].setImageResource(res);
     }
 
     
--- app/src/main/java/comschakravorti21/github/connectfourai/MainActivity.java
+++ app/src/main/java/comschakravorti21/github/connectfourai/MainActivity.java
@@ -1,16 +1,10 @@
 package comschakravorti21.github.connectfourai;
 
-import android.graphics.drawable.Drawable;
-import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-
-import static android.R.attr.tag;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,19 +12,20 @@ public class MainActivity extends AppCompatActivity {
     public static final int PLAYER_2 = 1;
     public static final int CPU = 3;
 
-    private GridLayout board;
-    private ImageButton[][] buttons;
+    private GridLayout gridBoard;
     private int[][] pieces;
     private int player;
+    public GameBoard gameboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        board = (GridLayout)findViewById(R.id.board);
-        buttons = new ImageButton[6][7];
-        pieces = new int[6][7];
+        gridBoard = (GridLayout)findViewById(R.id.board);
+        pieces = new int[GameBoard.ROWS][GameBoard.COLUMNS];
         player = PLAYER_1;
+
+        gameboard = new GameBoard();
 
         View.OnClickListener buttonClickListener = new View.OnClickListener() {
             @Override
@@ -39,16 +34,11 @@ public class MainActivity extends AppCompatActivity {
                 int col = Character.digit( ((String)button.getTag() ).charAt(1), 10) - 1;
 
                 //Start from bottom when searching where to insert piece
-                for(int i = buttons.length - 1; i >= 0; i--) {
+                for(int i = pieces.length - 1; i >= 0; i--) {
                     //Place the piece if its empty
                     if(pieces[i][col] == 0) {
-                        pieces[i][col] = 1;
 
-                        if(player == PLAYER_1) {
-                            buttons[i][col].setImageResource(R.mipmap.piece_yellow);
-                        } else {
-                            buttons[i][col].setImageResource(R.mipmap.piece_red);
-                        }
+                        gameboard.placePiece(i, col, player);
 
                         player = -player;
                         break;
@@ -57,14 +47,15 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        for(int i = 0; i < board.getChildCount(); i++) {
-            ImageButton button = (ImageButton)board.getChildAt(i);
+
+            for(int i = 0; i < gridBoard.getChildCount(); i++) {
+            ImageButton button = (ImageButton) gridBoard.getChildAt(i);
             button.setImageResource(R.mipmap.piece_empty);
             button.setOnClickListener(buttonClickListener);
             String tag = (String)button.getTag();
             int row = Character.digit(tag.charAt(0), 10);
             int col = Character.digit(tag.charAt(1), 10);
-            buttons[row-1][col-1] = button;
+            gameboard.setButtonImg(row-1, col-1, button);
         }
 
     }
