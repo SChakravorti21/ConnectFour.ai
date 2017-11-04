e1fc3bfc063b5bf2c7e98111200f0e55f917ee80
yoloducks <atharva.kench@gmail.com>
2017-11-03 23:25:12 -0400

    added connectboard class

---
+++ app/src/main/java/comschakravorti21/github/connectfourai/ConnectBoard.java
@@ -0,0 +1,22 @@
+package comschakravorti21.github.connectfourai;
+
+/**
+ * Created by Athu on 11/3/2017.
+ */
+
+public class ConnectBoard {
+
+    public static final int ROWS = 6;
+    public static final int COLUMNS = 7;
+    private int[][] gameState;
+    //0 = empty, 1 = blue, 2 = red
+
+
+    public ConnectBoard() {
+        for(int i = 0; i < ROWS; i++) {
+            for(int j = 0; j < COLUMNS; j++) {
+                gameState[i][j] = 0;
+            }
+        }
+    }
+}
