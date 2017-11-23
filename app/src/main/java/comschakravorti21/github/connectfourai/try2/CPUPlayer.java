dca69f8c336d304eb8d7b037f1810dff34d8acee
Development <Development@Shoumyos-MBP.home>
2017-11-23 14:23:22 -0500

    Minimax working better

---
+++ app/src/main/java/comschakravorti21/github/connectfourai/try2/CPUPlayer.java
@@ -0,0 +1,218 @@
+package comschakravorti21.github.connectfourai.try2;
+
+import comschakravorti21.github.connectfourai.MainActivity;
+
+/**
+ * Created by Development on 11/23/17.
+ */
+
+public class CPUPlayer {
+
+    public static final int MAX_DEPTH = 7;
+    public static final int MAX_SCORE = 1000000;
+
+    public int minimax(int[][] currentState, int player) {
+        int ret = (int)(Math.random()*7);
+        double eval = 0;
+        double highestEval = 0;
+
+        for(int i = 0; i < Gameboard2.COLUMNS; i++) {
+            int row = Gameboard2.rowIfPlaced(i, currentState);
+            if(row != -1) {
+                currentState[row][i] = player;
+                eval = minimaxRec(currentState, player, true, 1);
+
+                if(eval > highestEval) {
+                    highestEval = eval;
+                    ret = i;
+                }
+
+                currentState[row][i] = 0; //undo the move that was previously done
+            }
+        }
+
+        return ret;
+    }
+
+    //Returns the static evaluations from levels 1 through depth (level 0 being root)
+    public double minimaxRec(int[][] currentState, int player, boolean maximizing, int depth) {
+        if(depth == MAX_DEPTH) {
+            return staticEval(currentState, player);
+        }
+
+        double bestResult = 0;
+        double eval = 0;
+        int newPlayer = (player == MainActivity2.PLAYER_1) ?
+                MainActivity2.PLAYER_2 : MainActivity2.PLAYER_1; //The player on next round
+
+        if(maximizing) {
+            //int highestEvalCol = (int)Math.random()*7; //pick random column as default
+            double highestEval = 0;
+
+            for(int i = 0; i < Gameboard2.COLUMNS; i++) {
+                int row = Gameboard2.rowIfPlaced(i, currentState);
+                if(row != -1) {
+                    currentState[row][i] = player;
+                    eval = minimaxRec(currentState, newPlayer, !maximizing, depth+1);
+
+                    if(eval > highestEval) {
+                        highestEval = eval;
+                    }
+
+                    currentState[row][i] = 0; //undo the move that was previously done
+                }
+            }
+
+            bestResult = highestEval;
+        } else {
+            //int highestEvalCol = (int)Math.random()*7; //pick random column as default
+            double lowestEval = 0;
+
+            for(int i = 0; i < Gameboard2.COLUMNS; i++) {
+                int row = Gameboard2.rowIfPlaced(i, currentState);
+                if(row != -1) {
+                    currentState[row][i] = player;
+                    eval = minimaxRec(currentState, newPlayer, !maximizing, depth+1);
+
+                    if(eval < lowestEval) {
+                        lowestEval = eval;
+                    }
+
+                    currentState[row][i] = 0; //undo the move that was previously done
+                }
+            }
+
+            bestResult = lowestEval;
+        }
+
+        return bestResult;
+    }
+
+    public double staticEval(int[][] currentState, int player) {
+        //THIS IS CURRENTLY A VERY NAIVE FORM OF STATIC EVALUATION, BUT IS
+        //USEFUL FOR TESTING NONETHELESS
+
+        //If we are evaluating for human player (PLAYER_1), then we'll
+        //multiply the eval by -1 (minimizing human player score), otherwise
+        //leave score unaltered
+        int multiplier = (player == MainActivity2.PLAYER_1) ? -1 : 1;
+        double ret = 0;
+        int maxInRow = 0;
+
+        /*
+            Start with primitive evaluation –– # of 3 in a row * 20 * mult
+            If there are 4 in a row, return 1000 (game over)
+         */
+
+        //Check each row individually for # of consecutive pieces
+        int deltaX = 1;
+        int deltaY = 1;
+        for(int row = 0; row < Gameboard2.ROWS; row += deltaY, maxInRow = 0) {
+            for(int col = 0; col < Gameboard2.COLUMNS; col += deltaX) {
+
+                //If there is a win, there is no point in checking anymore
+                if (Gameboard2.checkWin(row, col, currentState, player)) {
+                    return (multiplier * MAX_SCORE);
+                }
+
+                //If we find a piece belonging to player, increment maxInRow
+                if (currentState[row][col] == player) {
+                    maxInRow++;
+                }
+                //If we don;t find a piece, then calculate the associated value
+                //with the running max (not including single pieces)
+                else if(maxInRow > 1){
+                    ret += Math.pow(10, maxInRow);
+                    maxInRow = 0;
+                } else {
+                    maxInRow = 0;
+                }
+            }
+
+            //It is possible that traversing through the column to the bottom never results
+            //in a '0' position, so be sure to add remainder
+            if(maxInRow != 0){
+                ret += Math.pow(10, maxInRow);
+            }
+        }
+        maxInRow = 0; //Reset max in row for next iteration set
+
+        //Check each column individually for # of consecutive pieces
+        for(int col = 0; col < Gameboard2.COLUMNS; col += deltaX, maxInRow = 0) {
+            for(int row = 0; row < Gameboard2.ROWS; row += deltaY) {
+
+                if (Gameboard2.checkWin(row, col, currentState, player)) {
+                    return (multiplier * MAX_SCORE);
+                }
+
+                if (currentState[row][col] == player) {
+                    maxInRow++;
+                } else if(maxInRow > 1){
+                    ret += Math.pow(10, maxInRow);
+                    maxInRow = 0;
+                } else {
+                    maxInRow = 0;
+                }
+            }
+
+            if(maxInRow != 0){
+                ret += Math.pow(10, maxInRow);
+            }
+        }
+        maxInRow = 0;
+
+        //Checking diagonally (top-left t0 bottom-right direction scenarios)
+        int[] startingRows = new int[]{2, 1, 0, 0, 0, 0};
+        int[] startingCols = new int[]{0, 0, 0, 1, 2, 3};
+        for(int i = 0; i < startingRows.length && i < startingCols.length; i++) {
+            for(int row = startingRows[i], col = startingCols[i]; row < Gameboard2.ROWS && col < Gameboard2.COLUMNS;
+                row += deltaY, col += deltaX) {
+
+                if (Gameboard2.checkWin(row, col, currentState, player)) {
+                    return (multiplier * MAX_SCORE);
+                }
+
+                if (currentState[row][col] == player) {
+                    maxInRow++;
+                } else if(maxInRow > 1){
+                    ret += Math.pow(10, maxInRow);
+                    maxInRow = 0;
+                } else {
+                    maxInRow = 0;
+                }
+            }
+
+            if(maxInRow != 0){
+                ret += Math.pow(10, maxInRow);
+            }
+        }
+
+        //Checking diagonally (top-right t0 bottom-left direction scenarios)
+        deltaY = -1;
+        startingRows = new int[]{3, 4, 5, 5, 5, 5};
+        for(int i = 0; i < startingRows.length && i < startingCols.length; i++) {
+            for(int row = startingRows[i], col = startingCols[i]; row >= 0 && col < Gameboard2.COLUMNS;
+                row += deltaY, col += deltaX) {
+
+                if (Gameboard2.checkWin(row, col, currentState, player)) {
+                    return (multiplier * MAX_SCORE);
+                }
+
+                if (currentState[row][col] == player) {
+                    maxInRow++;
+                } else if(maxInRow > 1){
+                    ret += Math.pow(10, maxInRow);
+                    maxInRow = 0;
+                } else {
+                    maxInRow = 0;
+                }
+            }
+
+            if(maxInRow != 0){
+                ret += Math.pow(10, maxInRow);
+            }
+        }
+        return ret*multiplier;
+
+    }
+}
--- app/src/main/java/comschakravorti21/github/connectfourai/try2/MainActivity2.java
+++ app/src/main/java/comschakravorti21/github/connectfourai/try2/MainActivity2.java
@@ -1,5 +1,7 @@
 package comschakravorti21.github.connectfourai.try2;
 
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,7 +11,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.lang.reflect.GenericArrayType;
+import java.util.concurrent.Executor;
 
 import comschakravorti21.github.connectfourai.R;
 
@@ -20,11 +26,14 @@ public class MainActivity2 extends AppCompatActivity {
     public static final int CPU = 3;
 
     private Toolbar toolbar;
+    private ProgressBar progressBar;
     private GridLayout gridBoard;
     //private int[][] pieces;
     private int player;
     public Gameboard2 gameboard;
     private int scoreP1, scoreP2;
+    private CPUPlayer cpu;
+    boolean waitingForGeneration;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -49,16 +58,20 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         gridBoard = findViewById(R.id.board);
         //pieces = new int[Gameboard2.ROWS][Gameboard2.COLUMNS];
         player = PLAYER_1;
         scoreP1 = scoreP2 = 0;
+        cpu = new CPUPlayer();
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        progressBar = findViewById(R.id.progress);
+        progressBar.setVisibility(View.GONE);
 
         gameboard = new Gameboard2();
+        waitingForGeneration = false;
 
         //No need to redefine the OnClickListener for every button, so we can just use the
         //same one
@@ -79,23 +92,89 @@ public class MainActivity2 extends AppCompatActivity {
 
 
     private class ButtonClickListener implements View.OnClickListener {
+        Executor executor;
+        Runnable run;
+
+        public ButtonClickListener() {
+            executor = new Executor() {
+                @Override
+                public void execute(@NonNull Runnable runnable) {
+                    new Thread(runnable).start();
+                }
+            };
+
+            run = new Runnable() {
+                @Override
+                public void run() {
+                    final int bestCol = cpu.minimax(gameboard.getBoard(), player); //get the best move for CPU
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+
+                    final int row = Gameboard2.rowIfPlaced(bestCol, gameboard.getBoard());
+                    if(row != -1) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                //Idk why this doesn't work when param is just "player"
+                                gameboard.placePiece(row, bestCol, PLAYER_2);
+                            }
+                        });
+
+                        Log.d("Static eval for player " + player,
+                                "" + cpu.staticEval(gameboard.getBoard(), player));
+
+                        if(Gameboard2.checkWin(row, bestCol, gameboard.getBoard(), player)) {
+                            Log.d("Check Win", "TRUE");
+                            if(player == PLAYER_1) {
+                                scoreP1++;
+                                TextView scoreView = (TextView)findViewById(R.id.score_P1);
+                                scoreView.setText("" + scoreP1);
+                            } else {
+                                scoreP2++;
+                                TextView scoreView = (TextView)findViewById(R.id.score_P2);
+                                scoreView.setText("" + scoreP2);
+                            }
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    gameboard.resetBoard();
+                                }
+                            });
+                        }
+                    }
+
+                    player = PLAYER_1; //Swap player
+                    waitingForGeneration = false; //Move generation is complete, user can play again
+                }
+            };
+        }
+
         @Override
         public void onClick(View view) {
+            if(waitingForGeneration)
+                return;
+
             ImageButton button = (ImageButton)view;
+            waitingForGeneration = true;
 
             //Button tags were defined as Strings with "rowcol" information, so getting
             //the second character gives the column
             int col = Character.digit( ((String)button.getTag() ).charAt(1), 10) - 1;
 
             //Start from bottom when searching where to insert piece
-            int row = gameboard.rowIfPlaced(col, gameboard.getBoard());
+            int row = Gameboard2.rowIfPlaced(col, gameboard.getBoard());
             if(row != -1) {
                 gameboard.placePiece(row, col, player);
 
                 Log.d("Static eval for player " + player,
-                        "" + Gameboard2.staticEval(gameboard.getBoard(), player));
+                        "" + cpu.staticEval(gameboard.getBoard(), player));
 
-                if(gameboard.checkWin(row, col, gameboard.getBoard(), player)) {
+                if(Gameboard2.checkWin(row, col, gameboard.getBoard(), player)) {
                     Log.d("Check Win", "TRUE");
                     if(player == PLAYER_1) {
                         scoreP1++;
@@ -110,8 +189,13 @@ public class MainActivity2 extends AppCompatActivity {
                     gameboard.resetBoard();
                 }
 
-                player = (player == PLAYER_1) ? PLAYER_2 : PLAYER_1;
             }
+
+            player = PLAYER_2; //Swap player
+
+            //Set the progress bar to be visible to prevent it from  looking like UI freeze
+            progressBar.setVisibility(View.VISIBLE);
+            executor.execute(run);
         }
     }
 }
