b9152efb5473f1f404a51fefc74e6cabfdb6cc28
Development <Development@Shoumyos-MBP.home>
2017-11-25 16:19:03 -0500

    Documented MainActivity2 class

--- app/src/main/java/comschakravorti21/github/connectfourai/try2/MainActivity2.java
+++ app/src/main/java/comschakravorti21/github/connectfourai/try2/MainActivity2.java
@@ -1,5 +1,8 @@
 package comschakravorti21.github.connectfourai.try2;
 
+/**
+ * Created by Shoumyo Chakravorti on 11/21/17.
+ */
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,7 +22,14 @@ import comschakravorti21.github.connectfourai.R;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    /**
+     * Player 1's corresponding player number in game states
+     */
     public static final int PLAYER_1 = 1;
+
+    /**
+     * Player 2's corresponding player number in game states
+     */
     public static final int PLAYER_2 = 2;
 
     private Toolbar toolbar;
@@ -31,10 +41,17 @@ public class MainActivity2 extends AppCompatActivity {
     private int scoreP1, scoreP2;
     private boolean generatingMove;
 
+    /**
+     * Overrides AppCompatActivity's onOptionsItemsSelected method.
+     * Currently the only option is to reset the game.
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
             case R.id.action_reset:
+                //If user wishes to restart the game, reset the board and scores
                 gameboard.resetBoard();
                 TextView scoreView1 = findViewById(R.id.score_P1);
                 scoreView1.setText("0");
@@ -45,25 +62,37 @@ public class MainActivity2 extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Overrides AppCompatActivity's onCreateOptionsMenu method.
+     * Inflates the toolbar menu
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.action_bar_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    /**
+     * Overrides AppCompatActivity's onCreate method.
+     * Instantiates all fields and attaches listeners to game board buttons
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         gridBoard = findViewById(R.id.board);
-        //pieces = new int[Gameboard2.ROWS][Gameboard2.COLUMNS];
         player = PLAYER_1;
         scoreP1 = scoreP2 = 0;
         generatingMove = false;
         cpu = new CPUPlayer();
 
+        //Set the toolbar
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //Progress bar to display during CPU move generation
         progressBar = findViewById(R.id.progress);
         progressBar.setVisibility(View.GONE);
 
@@ -74,6 +103,8 @@ public class MainActivity2 extends AppCompatActivity {
         View.OnClickListener buttonClickListener = new ButtonClickListener();
 
 
+        //Get all ImageButtons, set their image resources to empty cell images, attach
+        //listeners, and use the button tag to add the button to the gameboard object
         for(int i = 0; i < gridBoard.getChildCount(); i++) {
             ImageButton button = (ImageButton) gridBoard.getChildAt(i);
             button.setImageResource(R.mipmap.piece_empty);
@@ -86,26 +117,37 @@ public class MainActivity2 extends AppCompatActivity {
 
     }
 
-
+    /**
+     *  A private class implementing the View.OnClickListener interface to define
+     *  game board button behavior.
+     */
     private class ButtonClickListener implements View.OnClickListener {
+        /**
+         * Overrides View's onClick method. Since the only buttons are ones on the game
+         * board, this method parses the button's row and column to place the user's piece.
+         * Afterwards, a MoveGenerator AsyncTask is used to generate the CPU move.
+         * @param view
+         */
         @Override
         public void onClick(View view) {
+            //Do not allow player to do anything while CPU is generating a move
             if(generatingMove)
                 return;
 
+            //All buttons are ImageButtons
             ImageButton button = (ImageButton)view;
 
             //Button tags were defined as Strings with "rowcol" information, so getting
             //the second character gives the column
-            int col = Character.digit( ((String)button.getTag() ).charAt(1), 10) - 1;
+            int col = Character.digit( ((String)button.getTag()).charAt(1), 10) - 1;
 
-            //Start from bottom when searching where to insert piece
+            //If this column can be played in, get the respective row to play in
             int row = Gameboard2.rowIfPlaced(col, gameboard.getBoard());
             if(row != -1) {
                 gameboard.placePiece(row, col, player);
 
+                //Check for a win
                 if(Gameboard2.checkWin(row, col, gameboard.getBoard(), player)) {
-                    Log.d("Check Win", "TRUE");
                     if(player == PLAYER_1) {
                         scoreP1++;
                         TextView scoreView = (TextView)findViewById(R.id.score_P1);
@@ -116,43 +158,64 @@ public class MainActivity2 extends AppCompatActivity {
                         scoreView.setText("" + scoreP2);
                     }
 
+                    //reset the board if someone wins
                     gameboard.resetBoard();
                 }
 
+                player = PLAYER_2; //Swap player
+
+                //If the board fills up, clear it
+                if(gameboard.isFull()) {
+                    gameboard.resetBoard();
+                }
+
+                //Set global variable to true when a move is being generated
+                //Prevents user from spamming buttons while CPU is generating a move
+                generatingMove = true;
+                MoveGenerator moveGenerator = new MoveGenerator();
+                moveGenerator.execute();
             }
-
-            player = PLAYER_2; //Swap player
-
-            //If the board fills up, clear it
-            if(gameboard.isFull()) {
-                gameboard.resetBoard();
-            }
-
-            generatingMove = true;
-            MoveGenerator moveGenerator = new MoveGenerator();
-            moveGenerator.execute();
         }
     }
 
+    /**
+     * Private class extending AsyncTask to generate CPU moves. An AsyncTask was used
+     * in order to update progress on the screen and prevent it from looking
+     * like UI freeze.
+     */
     private class MoveGenerator extends AsyncTask<Void, Void, Void> {
         private int row;
         private int bestCol;
 
+        /**
+         * Instantiates MoveGenerator with dummy row and column values.
+         */
         public MoveGenerator() {
+            //Negative values used for debugging
             row = -1;
             bestCol = -1;
         }
 
+        /**
+         * Indicates to user that a move generation process will begin
+         */
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            //Set the progress bar to visible because we will begin the move generation
             progressBar.setVisibility(View.VISIBLE);
         }
 
+        /**
+         * Decides the best move for CPU to play.
+         * @param voids list of voids (required by AsyncTask))
+         * @return
+         */
         @Override
         protected Void doInBackground(Void... voids) {
             long start = System.nanoTime();
 
+            //Generate the best move and log how long it took
             bestCol = cpu.generateMove(gameboard.getBoard());
 
             Log.d("Calculation time", "" + (System.nanoTime() - start)/1000000 + " milliseconds");
@@ -162,12 +225,19 @@ public class MainActivity2 extends AppCompatActivity {
             return null;
         }
 
+        /**
+         * Updates the game board with the best move that the CPU chose.
+         * @param aVoid void parameter (required by AsyncTask)
+         */
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
 
-            Log.d("Post execute", "Running now");
+            //Only allow CPU to play a move after a move is generated
+            //This check was included because onPostExecute seems to update the
+            //UI multiple times sometimes.
             if(generatingMove) {
+                //Same functionality as when the user plays a piece
                 if (row != -1) {
                     gameboard.placePiece(row, bestCol, PLAYER_2);
 
@@ -199,6 +269,7 @@ public class MainActivity2 extends AppCompatActivity {
                     gameboard.resetBoard();
                 }
 
+                //Move generation is complete, allow user to make a move
                 progressBar.setVisibility(View.GONE);
                 generatingMove = false;
             }
